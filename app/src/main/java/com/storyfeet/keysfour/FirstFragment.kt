ce333321ec56967d4d4b7074f39e7f5c2a4f7218
Matthew Stoodley <matt.storyfeet@gmail.com>
2024-12-30 17:55:35 +0000

    now has working enable keyboard buttons

--- app/src/main/java/com/storyfeet/keysfour/FirstFragment.kt
+++ app/src/main/java/com/storyfeet/keysfour/FirstFragment.kt
@@ -1,13 +1,18 @@
 package com.storyfeet.keysfour
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.storyfeet.keysfour.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -36,9 +41,22 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
-        val introView: WebView = view.findViewById<WebView>(R.id.web_intro)
-        introView.loadUrl("file:///android_asset/intro.html")
+        binding.webIntro.loadUrl("file:///android_asset/intro.html")
 
+        binding.buttonEnable.setOnClickListener{
+            val imeIntent = Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+            startActivity(imeIntent);
+        }
+
+        binding.buttonSelectIme.setOnClickListener{
+            val imeManager =
+                requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            if (imeManager != null) {
+                imeManager.showInputMethodPicker()
+            } else {
+                Toast.makeText(context, "Error", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
